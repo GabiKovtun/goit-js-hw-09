b3186059b95c247f9313fd4200d19f42e4eeaa6e
GabiKovtun <gabi.tovt@gmail.com>
2023-03-06 12:55:15 +0100

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+function getRandomHexColor() {
+  return #${Math.floor(Math.random() * 16777215).toString(16)};
+}
+
+startChangeColor = () => {
+  console.log('start');
+  changeId = setInterval(() => {
+    bodyEl.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  buttonStart.disabled = true;
+};
+stopChangeColor = () => {
+  buttonStart.disabled = false;
+  clearInterval(changeId);
+};
+
+buttonStart.addEventListener('click', startChangeColor);
+
+buttonStop.addEventListener('click', stopChangeColor);
