373e99a654297a90496fde89b08f1eca580428ee
GabiKovtun <gabi.tovt@gmail.com>
2023-03-06 13:31:28 +0100

    Fix color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,14 +6,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-startChangeColor = () => {
+let startChangeColor = () => {
   console.log('start');
   changeId = setInterval(() => {
     bodyEl.style.backgroundColor = getRandomHexColor();
   }, 1000);
   buttonStart.disabled = true;
 };
-stopChangeColor = () => {
+let stopChangeColor = () => {
   buttonStart.disabled = false;
   clearInterval(changeId);
 };
